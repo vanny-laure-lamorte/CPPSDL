ccdf9a880c414cb663c25b40fc42de5183348f0a
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-09-02 12:16:14 +0200

    <init>[all files]tests game logic

--- SDL/main.cpp
+++ SDL/main.cpp
@@ -1,7 +1,41 @@
+using namespace std;
+
 #include <iostream>
+
+// Function prototypes
+void createGrid(int grille[4][4]);
+void displayGrid(int grille[4][4]);
+
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Salut Les copains";
+    int grid[4][4];
+
+    // Initial Grid
+    createGrid(grid); 
+
+    // Display Grid
+    displayGrid(grid);
+
     return 0;
+}
+
+void createGrid(int grid[4][4]){
+ for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            grid[i][j] = 0; 
+        }
+    }
+}
+
+void displayGrid (int grid[4][4]) {
+    cout << "Grille 4x4 :" ; endl
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << grid[i][j] << " "; 
+        }
+        cout << endl;  
+    }
+
 }
---
+++ SDL/src/include/Element.hpp
@@ -0,0 +1,40 @@
+#ifndef ELEMENT_HPP
+#define ELEMENT_HPP
+
+#include <iostream>
+using namespace std;
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_ttf.h"
+#include "SDL2/SDL_image.h"
+
+//*** RENDERER ***// 
+
+SDL_Renderer *renderer;
+
+//*** COLORS ***// 
+const SDL_Color COLOR_BLACK = {0, 0, 0, 255};
+const SDL_Color COLOR_WHITE = {255, 255, 255, 255};
+
+//*** IMAGES ***// 
+
+//*** TEXTURES ***// 
+
+//*** METHODES ***// 
+
+void unloadImgGame() {
+   
+}
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
