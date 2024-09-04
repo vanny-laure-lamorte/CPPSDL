38c6b3a56ea4b35df02ff531f9513fc63699d76e
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-09-04 09:38:42 +0200

    <test>[sfmltemplatearchitecture]:<testing the compilation with cmake>

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -1,9 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <direct.h>
 
 int main()
 {
+    // Print the current working directory
+    char buffer[MAX_PATH];
+    _getcwd(buffer, MAX_PATH);
+    std::cout << "Current working directory: " << buffer << std::endl;
+    
     auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
     window.setFramerateLimit(144);
+
+    // Load a sprite to display
+    sf::Texture texture;
+    if (!texture.loadFromFile("C:\\Users\\tslem\\Desktop\\Laplateforme\\bachelore2\\depot\\cpp\\projetGroupe\\CPPSDL\\SFML\\assets\\img\\wallhaven-p8rmm9.jpg"))
+        return EXIT_FAILURE;
+    sf::Sprite sprite(texture);
+
+    // Load a font
+    sf::Font font;
+    if (!font.loadFromFile("C:\\Users\\tslem\\Desktop\\Laplateforme\\bachelore2\\depot\\cpp\\projetGroupe\\CPPSDL\\SFML\\assets\\fonts\\Super Caramel.ttf"))
+    return EXIT_FAILURE;
+    sf::Text text("Hello SFML", font, 50);
 
     while (window.isOpen())
     {
@@ -16,6 +37,10 @@ int main()
         }
 
         window.clear();
+        window.draw(text);
+        window.draw(sprite);
         window.display();
     }
+
+    return EXIT_SUCCESS;
 }
