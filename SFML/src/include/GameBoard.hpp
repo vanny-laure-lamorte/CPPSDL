49fc74e73a13e9af11ca03509fe6a9033c74fc32
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-09-16 10:54:24 +0200

    <debug>[gameBoard] fixed score calculation

--- SFML/src/include/GameBoard.hpp
+++ SFML/src/include/GameBoard.hpp
@@ -4,24 +4,26 @@
 #include <vector>
 #include "Tile.hpp"
 
-class GameBoard {
+class GameBoard
+{
     /*
-    * The GameBoard class represents the game board of the 2048 game.
-    * The game board is a square grid with a size of 4x4.
-    * The game board is made up of tiles.
-    * The game board can be displayed.
-    * The game board can be shifted left, right, up, or down.
-    * The game board can have a random tile added.
-    * The game board can be checked if it is full.
-    * The game board can be checked if it can move.
-    */
+     * The GameBoard class represents the game board of the 2048 game.
+     * The game board is a square grid with a size of 4x4.
+     * The game board is made up of tiles.
+     * The game board can be displayed.
+     * The game board can be shifted left, right, up, or down.
+     * The game board can have a random tile added.
+     * The game board can be checked if it is full.
+     * The game board can be checked if it can move.
+     */
 private:
     int size;
+    int score = 0;
 
 public:
     GameBoard(int size = 4);
-    bool shiftLeft(std::vector<Tile>& line);
-    bool mergeTiles(std::vector<Tile>& line);
+    bool shiftLeft(std::vector<Tile> &line);
+    bool mergeTiles(std::vector<Tile> &line);
     void display() const;
     bool moveLeft();
     bool moveRight();
@@ -32,7 +34,6 @@ public:
     bool canMove() const;
     std::vector<std::vector<Tile>> tiles;
     int getScore() const;
-
 };
 
 #endif // GameBoard_HPP
